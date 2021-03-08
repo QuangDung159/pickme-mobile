b81b10444ae5928a349099d572ab98c728a3a910
Dung Lu <quangdunglu159@gmail.com>
2021-03-08 23:08:27 +0700

    Add

--- src/screens/Personal.jsx
+++ src/screens/Personal.jsx
@@ -402,7 +402,7 @@ export default function Personal(props) {
                     />
                 </Block>
             ) : (
-                <Block>
+                <Block flex>
                     <ScrollView
                         showsVerticalScrollIndicator={false}
                     >
