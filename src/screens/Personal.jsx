fc17d8c695a2d362975de635cb4cad7f406ea7a7
Dung Lu <quangdunglu159@gmail.com>
2021-03-08 23:00:30 +0700

    Add

--- src/screens/Personal.jsx
+++ src/screens/Personal.jsx
@@ -3,7 +3,7 @@ import {
 } from 'galio-framework';
 import React, { useEffect, useState } from 'react';
 import {
-    ActivityIndicator, Alert,
+    ActivityIndicator,
     Image,
     ScrollView,
     StyleSheet
@@ -21,7 +21,6 @@ import { resetStoreSignOut, setCurrentUser } from '../redux/Actions';
 import { rxUtil } from '../utils';
 
 const thumbMeasure = (NowTheme.SIZES.WIDTH_BASE * 0.85) / 3;
-const marginValue = ((NowTheme.SIZES.WIDTH_BASE * 0.9) - thumbMeasure * 3) / 2;
 
 export default function Personal(props) {
     const { navigation } = props;
@@ -30,7 +29,6 @@ export default function Personal(props) {
     const [visible, setVisible] = useState(false);
     const [imageIndex, setImageIndex] = useState(0);
     const [listImageReview, setListImageReview] = useState([]);
-    const [listImageDisplay, setListImageDisplay] = useState([]);
 
     const token = useSelector((state) => state.userReducer.token);
     const currentUser = useSelector((state) => state.userReducer.currentUser);
@@ -56,7 +54,6 @@ export default function Personal(props) {
             headers,
             (res) => {
                 setListImageReview(res.data.data);
-                setListImageDisplay(res.data.data);
             },
             () => {},
             () => {}
@@ -99,89 +96,12 @@ export default function Personal(props) {
         MediaHelpers.pickImage(true, [1, 1], (result) => handleOnPickAvatar(result.uri));
     };
 
-    const onClickUploadProfileImage = () => {
-        MediaHelpers.pickImage(false, [], (result) => handleUploadImageProfile(result.uri));
-    };
-
     const onSignOut = () => {
         navigation.reset({
             index: 0,
             routes: [{ name: ScreenName.ONBOARDING }],
         });
         dispatch(resetStoreSignOut());
-    };
-
-    const onLongPressImage = (imageId) => {
-        Alert.alert(
-            'Xác nhận',
-            'Bạn muốn xoá tấm ảnh này?',
-            [
-                {
-                    text: 'Huỷ',
-                    style: 'cancel'
-                },
-                { text: 'Xoá', onPress: () => removeImage(imageId) }
-            ],
-            { cancelable: false }
-        );
-    };
-
-    const removeImage = (imageId) => {
-        const headers = {
-            Authorization: token
-        };
-
-        setIsShowSpinner(true);
-
-        MediaHelpers.removeImage(
-            `${Rx.USER.REMOVE_PROFILE_IMAGE}/${imageId}`,
-            headers,
-            (res) => {
-                ToastHelpers.renderToast(
-                    res?.data?.message || 'Xoá ảnh thành công!', 'success'
-                );
-
-                getListImagesByUser();
-                setIsShowSpinner(false);
-            },
-            (err) => {
-                ToastHelpers.renderToast(
-                    err?.data?.message || 'Xoá ảnh thất bại! Vui lòng thử lại.', 'error'
-                );
-                setIsShowSpinner(false);
-            },
-            () => {
-                ToastHelpers.renderToast('Xoá ảnh thất bại! Vui lòng thử lại.', 'error');
-                setIsShowSpinner(false);
-            }
-        );
-    };
-
-    const handleUploadImageProfile = (uri) => {
-        setIsShowSpinner(true);
-
-        MediaHelpers.uploadImage(
-            uri,
-            Rx.USER.UPLOAD_PROFILE_IMAGE,
-            token,
-            (res) => {
-                ToastHelpers.renderToast(
-                    res?.data?.message || 'Tải ảnh lên thành công!', 'success'
-                );
-                setIsShowSpinner(false);
-                getListImagesByUser();
-            },
-            (err) => {
-                ToastHelpers.renderToast(
-                    err?.data?.message || 'Tải ảnh lên thất bại! Vui lòng thử lại.', 'error'
-                );
-                setIsShowSpinner(false);
-            },
-            () => {
-                ToastHelpers.renderToast('Tải ảnh lên thất bại! Vui lòng thử lại.', 'error');
-                setIsShowSpinner(false);
-            }
-        );
     };
 
     // Render \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
@@ -266,107 +186,6 @@ export default function Personal(props) {
                                 </Block>
                             </TouchableWithoutFeedback>
                         </Block>
-
-                        {/* partner */}
-                        <Block
-                            flex={7}
-                            row
-                        >
-                            <Block flex>
-                                <Block
-                                    middle
-                                    style={{
-                                        padding: 10
-                                    }}
-                                >
-                                    <Text
-                                        size={NowTheme.SIZES.FONT_INFO}
-                                        color={NowTheme.COLORS.ACTIVE}
-                                        style={{
-                                            marginBottom: 4,
-                                            fontFamily: NowTheme.FONT.MONTSERRAT_BOLD
-                                        }}
-                                    >
-                                        {currentUser.earningExpected}
-                                    </Text>
-                                    <Text
-                                        style={{ fontFamily: NowTheme.FONT.MONTSERRAT_REGULAR }}
-                                        size={NowTheme.SIZES.FONT_SUB_TITLE}
-                                        color={NowTheme.COLORS.DEFAULT}
-                                    >
-                                        Kim cương/p
-                                    </Text>
-                                </Block>
-
-                                <Block
-                                    middle
-                                    style={{
-                                        padding: 10
-                                    }}
-                                >
-                                    <Text
-                                        color={NowTheme.COLORS.ACTIVE}
-                                        size={NowTheme.SIZES.FONT_INFO}
-                                        style={{ marginBottom: 4, fontFamily: NowTheme.FONT.MONTSERRAT_BOLD }}
-                                    >
-                                        26
-                                    </Text>
-                                    <Text
-                                        style={{ fontFamily: NowTheme.FONT.MONTSERRAT_REGULAR }}
-                                        size={NowTheme.SIZES.FONT_SUB_TITLE}
-                                        color={NowTheme.COLORS.DEFAULT}
-                                    >
-                                        Đơn hẹn
-                                    </Text>
-                                </Block>
-                            </Block>
-
-                            <Block flex>
-                                <Block
-                                    middle
-                                    style={{
-                                        padding: 10
-                                    }}
-                                >
-                                    <Text
-                                        color={NowTheme.COLORS.ACTIVE}
-                                        size={NowTheme.SIZES.FONT_INFO}
-                                        style={{ marginBottom: 4, fontFamily: NowTheme.FONT.MONTSERRAT_BOLD }}
-                                    >
-                                        3k
-                                    </Text>
-                                    <Text
-                                        style={{ fontFamily: NowTheme.FONT.MONTSERRAT_REGULAR }}
-                                        size={NowTheme.SIZES.FONT_SUB_TITLE}
-                                        color={NowTheme.COLORS.DEFAULT}
-                                    >
-                                        Lượt thích
-                                    </Text>
-                                </Block>
-
-                                <Block
-                                    middle
-                                    style={{
-                                        padding: 10
-                                    }}
-                                >
-                                    <Text
-                                        color={NowTheme.COLORS.ACTIVE}
-                                        size={NowTheme.SIZES.FONT_INFO}
-                                        style={{ marginBottom: 4, fontFamily: NowTheme.FONT.MONTSERRAT_BOLD }}
-                                    >
-                                        4.8/5
-                                    </Text>
-                                    <Text
-                                        style={{ fontFamily: NowTheme.FONT.MONTSERRAT_REGULAR }}
-                                        size={NowTheme.SIZES.FONT_SUB_TITLE}
-                                        color={NowTheme.COLORS.DEFAULT}
-                                    >
-                                        Đánh giá
-                                    </Text>
-                                </Block>
-                            </Block>
-                        </Block>
                     </Block>
                     <Block>
                         {renderButtonPanel(navigation)}
@@ -401,18 +220,6 @@ export default function Personal(props) {
                 paddingBottom: 10
             }}
         >
-            {/* is partner */}
-            <TouchableWithoutFeedback
-                onPress={() => navigation.navigate(ScreenName.BUSY_CALENDAR)}
-            >
-                <IconCustom
-                    name="timetable"
-                    family={IconFamily.MATERIAL_COMMUNITY_ICONS}
-                    size={23}
-                    color={NowTheme.COLORS.ACTIVE}
-                />
-            </TouchableWithoutFeedback>
-
             <TouchableWithoutFeedback
                 onPress={() => navigation.navigate(ScreenName.WALLET)}
             >
@@ -542,121 +349,10 @@ export default function Personal(props) {
         </Block>
     );
 
-    const renderButtonAddPhoto = () => (
-        <TouchableWithoutFeedback
-            onPress={() => onClickUploadProfileImage()}
-        >
-            <Block
-                row
-                style={{
-                    alignItems: 'center',
-                    paddingVertical: 5
-                }}
-            >
-
-                <IconCustom
-                    name="add-a-photo"
-                    size={NowTheme.SIZES.FONT_BUTTON_TITLE}
-                    color={NowTheme.COLORS.ACTIVE}
-                    family={IconFamily.MATERIAL_ICONS}
-                />
-                <Text
-                    color={NowTheme.COLORS.ACTIVE}
-                    style={{
-                        fontFamily: NowTheme.FONT.MONTSERRAT_REGULAR,
-                        marginLeft: 10
-                    }}
-                    size={NowTheme.SIZES.FONT_BUTTON_TITLE}
-                >
-                    Thêm ảnh
-                </Text>
-            </Block>
-        </TouchableWithoutFeedback>
-    );
-
-    const renderAlbumItem = (imageItem, index) => (
-        <TouchableWithoutFeedback
-            onPress={() => {
-                setVisible(true);
-                setImageIndex(index);
-                setListImageReview(listImageDisplay);
-            }}
-            onLongPress={() => onLongPressImage(imageItem.id)}
-            key={imageItem.id}
-        >
-            <Block style={[
-                styles.shadow
-            ]}
-            >
-                <CenterLoader size="small" />
-                <Block
-                    style={{
-                        zIndex: 99,
-                        marginRight: (index + 1) % 3 !== 0 ? marginValue : 0,
-                        marginTop: index > 2 ? marginValue : 0
-                    }}
-                >
-                    <Image
-                        resizeMode="cover"
-                        source={{ uri: imageItem.url }}
-                        style={styles.albumThumb}
-                    />
-                </Block>
-            </Block>
-        </TouchableWithoutFeedback>
-    );
-
-    const renderAlbums = () => (
-        <Block
-            flex
-            style={{
-                paddingBottom: 20,
-                width: NowTheme.SIZES.WIDTH_90,
-                alignSelf: 'center'
-            }}
-        >
-            <Block>
-                {renderButtonAddPhoto()}
-                <Block>
-                    {listImageDisplay.length === 0 ? (
-                        <Block
-                            style={{
-                                alignItems: 'center',
-                                marginTop: 15
-                            }}
-                        >
-                            <Text
-                                color={NowTheme.COLORS.SWITCH_OFF}
-                                style={{
-                                    fontFamily: NowTheme.FONT.MONTSERRAT_REGULAR,
-                                }}
-                                size={NowTheme.SIZES.FONT_INFO}
-                            >
-                                Bạn chưa có ảnh
-                            </Text>
-                        </Block>
-                    ) : (
-                        <Block
-                            row
-                            style={{
-                                marginTop: 15,
-                                flexWrap: 'wrap',
-                            }}
-                        >
-                            {listImageDisplay.map(
-                                (imageItem, index) => renderAlbumItem(imageItem, index)
-                            )}
-                        </Block>
-                    )}
-                </Block>
-            </Block>
-        </Block>
-    );
-
     const renderButtonLogout = () => (
         <Block
             style={{
-                paddingBottom: 20,
+                paddingTop: 20,
                 width: NowTheme.SIZES.WIDTH_90,
                 alignSelf: 'center'
             }}
@@ -717,12 +413,6 @@ export default function Personal(props) {
                         {renderInfoPanel(currentUser, navigation)}
 
                         <Block>
-                            <Block style={{
-                                marginTop: 20
-                            }}
-                            >
-                                {renderAlbums()}
-                            </Block>
                             {renderButtonLogout(navigation)}
                         </Block>
                     </ScrollView>
