3f3fc0b9f2bc10697236afe858a0e133dbe97e13
Dung Lu <quangdunglu159@gmail.com>
2021-03-20 23:16:38 +0700

    Button upload image

--- src/screens/Support.jsx
+++ src/screens/Support.jsx
@@ -2,12 +2,13 @@ import {
     Block, Button, Input, Text
 } from 'galio-framework';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import ImageView from 'react-native-image-viewing';
 import { useSelector } from 'react-redux';
-import { CenterLoader, NoteText } from '../components/uiComponents';
-import { NowTheme, Rx } from '../constants';
-import { ToastHelpers } from '../helpers';
+import { CenterLoader, IconCustom, NoteText } from '../components/uiComponents';
+import { IconFamily, NowTheme, Rx } from '../constants';
+import { MediaHelpers, ToastHelpers } from '../helpers';
 import { rxUtil } from '../utils';
 
 export default function Support() {
@@ -19,6 +20,9 @@ export default function Support() {
         url: ''
     });
     const [isShowSpinner, setIsShowSpinner] = useState(false);
+    const [listImageReview, setListImageReview] = useState([]);
+    const [image, setImage] = useState();
+    const [visible, setVisible] = useState(false);
 
     const token = useSelector((state) => state.userReducer.token);
 
@@ -85,6 +89,54 @@ export default function Support() {
                 setIsShowSpinner(false);
             }
         );
+    };
+
+    const handleOnPickImageReport = (uri) => {
+        console.log('uri :>> ', uri);
+        setBugReportForm({ ...bugReportForm, url: uri });
+        setImage(uri);
+    };
+
+    const onClickUploadImageReport = () => {
+        MediaHelpers.pickImage(true, [1, 1], (result) => handleOnPickImageReport(result.uri));
+    };
+
+    const renderImageReport = () => (
+        <TouchableWithoutFeedback
+            onPress={() => {
+                setVisible(true);
+                setListImageReview([{ url: bugReportForm.url }]);
+            }}
+        >
+            {/* <Image
+                source={{
+                    uri: `file://${RNFS.DocumentDirectoryPath}Users/quangdunglu/Library/Developer/CoreSimulator/Devices/9BD6C292-C9FA-439E-89B5-13C34FA0DEFD/data/Containers/Data/Application/082491AC-4FFD-4CD0-8E7D-3E49D677326A/Library/Caches/ExponentExperienceData/%2540quangdunglu159%252Fcustomer-pick-me-mobile-v1/ImagePicker/36C68008-8020-4627-AF16-432E28D3D3C7.jpg`
+                }}
+            /> */}
+            <Text>{image}</Text>
+        </TouchableWithoutFeedback>
+    );
+
+    const renderImageView = () => {
+        const listImageObj = [];
+
+        listImageReview.forEach((item) => {
+            listImageObj.push({
+                uri: item.url
+            });
+        });
+
+        if (visible) {
+            return (
+                <ImageView
+                    images={listImageObj}
+                    imageIndex={0}
+                    visible={visible}
+                    onRequestClose={() => setVisible(false)}
+                />
+            );
+        }
+        return <></>;
     };
 
     const renderButtonPanel = () => (
@@ -163,17 +215,53 @@ export default function Support() {
         );
     };
 
+    const renderUploadImageReportButton = () => (
+        <Block
+            style={{
+                paddingVertical: 10
+            }}
+        >
+            <Block row>
+                <Text
+                    color={NowTheme.COLORS.ACTIVE}
+                    size={16}
+                    style={{
+                        fontFamily: NowTheme.FONT.MONTSERRAT_REGULAR
+                    }}
+                >
+                    Ảnh chụp màn hình:
+                </Text>
+
+                <TouchableWithoutFeedback
+                    onPress={() => onClickUploadImageReport()}
+                    containerStyle={{
+                        justifyContent: 'center',
+                        marginLeft: 10
+                    }}
+                >
+                    <IconCustom
+                        name="photo-camera"
+                        family={IconFamily.MATERIAL_ICONS}
+                        color={NowTheme.COLORS.DEFAULT}
+                        size={20}
+                    />
+                </TouchableWithoutFeedback>
+            </Block>
+        </Block>
+    );
+
     const renderBugReportForm = () => (
         <>
             {renderInputBugTitle()}
             {renderInputBugDescription()}
+            {renderUploadImageReportButton()}
+            {renderImageReport()}
             {renderButtonPanel()}
         </>
     );
 
     const renderInputBugTitle = () => (
         <Block
-            middle
             style={{
                 paddingTop: 10
             }}
@@ -209,7 +297,6 @@ export default function Support() {
 
     const renderInputBugDescription = () => (
         <Block
-            middle
             style={{
                 paddingTop: 10
             }}
@@ -281,6 +368,7 @@ export default function Support() {
                 <CenterLoader size="large" />
             ) : (
                 <>
+                    {renderImageView()}
                     <Block
                         row
                         style={[{
