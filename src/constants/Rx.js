06304c0f97174e97cf4d23830af5a399e992f01b
Dung Lu <quangdunglu159@gmail.com>
2021-05-07 13:02:29 +0700

    task-form-change-password

--- src/constants/Rx.js
+++ src/constants/Rx.js
@@ -23,7 +23,8 @@ export default {
         GENERATE_OTP_WHEN_CHANGE_DEVICE: '/users/CustomerGenerateOTPChangedDeviceId',
         SUBMIT_CHANGE_DEVICE_CONFIRM: '/users/CustomerChangedDeviceIdConfirm',
         GENERATE_OTP_WHEN_FORGOT_PASSWORD: '/users/CustomerForGotPasswordOTP',
-        SUBMIT_FORGOT_PASSWORD_CONFIRM: '/users/CustomerForGotPasswordConfirm'
+        SUBMIT_FORGOT_PASSWORD_CONFIRM: '/users/CustomerForGotPasswordConfirm',
+        SBUMIT_CHANGE_PASSWORD: '/users/changepassword'
     },
     BANK: {
         BANK_ACCOUNTS: '/users/BankAccounts',
