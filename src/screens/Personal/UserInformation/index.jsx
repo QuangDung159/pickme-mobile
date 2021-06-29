97af487446cd504e496ff13e84ca75422101a701
Dung Lu <dung.lu@kyanon.digital>
2021-06-29 09:31:49 +0700

    Add

--- src/screens/Personal/UserInformation/index.jsx
+++ src/screens/Personal/UserInformation/index.jsx
@@ -90,8 +90,10 @@ export default function UserInformation({ navigation }) {
         const result = await UserServices.fetchCurrentUserInfoAsync();
         const { data } = result;
 
+        const currentUserInfo = UserServices.mappingCurrentUserInfo(data.data);
+
         if (data) {
-            dispatch(setCurrentUser(data.data));
+            dispatch(setCurrentUser(currentUserInfo));
         }
         setIsShowSpinner(false);
         setRefreshing(false);
