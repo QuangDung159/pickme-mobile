43c2d3692c65209892a66b21b364b3733275b06a
Dung Lu <dung.lu@kyanon.digital>
2021-11-07 23:46:40 +0700

    Udpate build version

--- src/constants/App.js
+++ src/constants/App.js
@@ -1,5 +1,5 @@
 export default {
-    APP_VERSION_OTA: '2111011',
+    APP_VERSION_OTA: '2111071',
     FONT: {
         montserratRegular: 'montserrat-regular',
         montserratBold: 'montserrat-bold',
