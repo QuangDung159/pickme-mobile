39972c120c3498f190e6f7f2e4b41cd368711a9b
DungLu <quangdunglu159@gmail.com>
2021-11-14 17:03:46 +0000

    Master

--- src/constants/App.js
+++ src/constants/App.js
@@ -1,5 +1,5 @@
 export default {
-    APP_VERSION_OTA: '2111141',
+    APP_VERSION_OTA: '2111142',
     FONT: {
         montserratRegular: 'montserrat-regular',
         montserratBold: 'montserrat-bold',
