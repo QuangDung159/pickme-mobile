aaf6d6aab0570487ef151010f368691ba826b942
huy-vu <huyxvd@gmail.com>
2021-12-12 13:01:14 +0700

    ok

--- src/constants/Guide.js
+++ src/constants/Guide.js
@@ -1,4 +1,20 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable max-len */
-export const GUIDE_CONTENT = `I. Hướng dẫn
+export const GUIDE_CONTENT = `I. Hướng dẫn Đặt hẹn\n
+Yêu cầu: Tài khoản XÁC THỰC\n
+B1: Trang chủ (Icon ⌂)
+B2: Chọn đối tượng muốn hẹn (Nhắn tin trao đổi trước khi hẹn)
+B3: Điền thông tin cuộc hẹn và nhấn nút ĐẶT HẸN.
+B4: Chờ Host ĐỒNG Ý.
+B5: Thanh toán.
+B6: Hoàn tất và Đánh giá.
+
+**Trường hợp sau khoảng thời gian nếu Host KHÔNG PHẢN HỒI hệ thống sẽ tự động hủy cuộc hẹn. ~60p trước thời điểm bắt đầu đối với offline và ~20p đối với online
+
+**Trường hợp sau khoảng thời gian nếu KHÔNG THANH TOÁN hệ thống sẽ tự động hủy cuộc hẹn. ~30p trước thời điểm bắt đầu đối với offline và ~10p đối với online
+
+II. Hướng dẫn Nạp/Rút Coin
+B1: Vào trang cá nhân
+B2: Vào mục Ví Tiền
+B3: Chọn Nạp/Rút và làm theo hướng dẫn
 `;
