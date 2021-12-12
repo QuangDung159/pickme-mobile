49a1115301f511762a65da91c286a8513a5546e6
DungLu <quangdunglu159@gmail.com>
2021-12-12 17:43:16 +0000

    Optimize

--- src/constants/App.js
+++ src/constants/App.js
@@ -1,5 +1,5 @@
 export default {
-    APP_VERSION_OTA: '21121301',
+    APP_VERSION_OTA: '21121302',
     FONT: {
         montserratRegular: 'montserrat-regular',
         montserratBold: 'montserrat-bold',
