c0ed4bad6725f54fa1a0b3cb7146736c643e5630
Dung Lu <dung.lu@kyanon.digital>
2021-12-17 11:38:27 +0700

    Add

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -4,22 +4,22 @@
 const defaultImage = require('@assets/images/icon.png');
 const Onboarding = require('../../assets/images/splash.png');
 const RegisterBackground = require('../../assets/images/splash.png');
-const Logo = require('../../assets/images/logo.png');
-const BankTransfer = require('../../assets/images/bank-transfer-sample.jpeg');
+const Logo = require('../../assets/images/logo.webp');
+const BankTransfer = require('../../assets/images/bank-transfer-sample.webp');
 
-const Mess1 = require('../../assets/images/guideImages/mess-1.png');
-const Mess2 = require('../../assets/images/guideImages/mess-2.png');
-const Mess3 = require('../../assets/images/guideImages/mess-3.png');
-const Mess4 = require('../../assets/images/guideImages/mess-4.png');
-const Mess5 = require('../../assets/images/guideImages/mess-5.png');
-const Mess6 = require('../../assets/images/guideImages/mess-6.png');
+const Mess1 = require('../../assets/images/guideImages/mess-1.webp');
+const Mess2 = require('../../assets/images/guideImages/mess-2.webp');
+const Mess3 = require('../../assets/images/guideImages/mess-3.webp');
+const Mess4 = require('../../assets/images/guideImages/mess-4.webp');
+const Mess5 = require('../../assets/images/guideImages/mess-5.webp');
+const Mess6 = require('../../assets/images/guideImages/mess-6.webp');
 
-const Skype1 = require('../../assets/images/guideImages/skype-1.png');
-const Skype2 = require('../../assets/images/guideImages/skype-2.png');
-const Skype3 = require('../../assets/images/guideImages/skype-3.png');
-const Skype4 = require('../../assets/images/guideImages/skype-4.png');
-const Skype5 = require('../../assets/images/guideImages/skype-5.png');
-const Skype6 = require('../../assets/images/guideImages/skype-6.png');
+const Skype1 = require('../../assets/images/guideImages/skype-1.webp');
+const Skype2 = require('../../assets/images/guideImages/skype-2.webp');
+const Skype3 = require('../../assets/images/guideImages/skype-3.webp');
+const Skype4 = require('../../assets/images/guideImages/skype-4.webp');
+const Skype5 = require('../../assets/images/guideImages/skype-5.webp');
+const Skype6 = require('../../assets/images/guideImages/skype-6.webp');
 
 export default {
     Onboarding,
