4226ad7aeaac3b32af87642a9ba9ea7d19b7eeec
Huy Đẹp Trai <huyxvd@gmail.com>
2021-12-19 06:44:49 +0000

    Update disclaimer & faq

--- src/constants/Guide.js
+++ src/constants/Guide.js
@@ -13,8 +13,14 @@ export const GUIDE_CONTENT = `I. Lên lịch hẹn
 
 **Trường hợp sau khoảng thời gian nếu KHÔNG THANH TOÁN hệ thống sẽ tự động hủy cuộc hẹn. ~30p trước thời điểm bắt đầu đối với offline và ~10p đối với online
 
-II. Nạp/Rút Coin
+II. Nạp/Rút Xu
 1 / Vào trang cá nhân
 2 / Vào mục Ví
 3 / Chọn Nạp/Rút và làm theo hướng dẫn
+
+III. Dành cho Host & Khách hàng ***
+Để đảm bảo cho tính minh bạch cũng như sự an toàn của Host và Khách đối với trường hợp hẹn Trực tiếp xin hãy chọn những nơi công cộng đông người qua lại.
+Luôn luôn thể hiện sự tôn trọng đối phương(Host & Khách).
+Nghiêm cấm mọi hành vi xâm phạm về thân thể, sức khoẻ, danh dự và nhân phẩm hay bất kỳ hình thức đối xử nào khác xâm phạm thân thể, sức khỏe, xúc phạm danh dự, nhân phẩm.
+Mọi hành vi trái với Luật pháp đều sẽ bị trừng trị bới Pháp Luật. Mỗi cá nhân phải tự chịu trách nhiệm trước pháp luật với hành vi của mình.
 `;
