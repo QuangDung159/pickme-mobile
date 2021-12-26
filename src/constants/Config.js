7d40f37575f31031bd74d33f2b9c65c2fbc08908
Dung Lu <dung.lu@kyanon.digital>
2021-12-26 15:49:08 +0700

    Add

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -3,7 +3,7 @@ export const dev = {
     API_URL: 'https://pick-me-api-v1.herokuapp.com/api/v1',
     API_URL_BACKUP: 'https://pick-me-api-v1.herokuapp.com/api/v1',
     SOCKET_URL: '//apollo-graphql-realtime-chat.herokuapp.com',
-    IMGBB_API_KEY: 'ad7d452dafd347a5b7a743a230740cd4',
+    IMGBB_API_KEY: '78ce20a9868210f47bd14409d19dd78b',
     IMAGE_STORAGE_URL: 'https://api.imgbb.com/1',
     MAP_API_KEY: 'MAP_API_KEY',
 };
@@ -23,7 +23,7 @@ export const stg = {
     API_URL: 'https://pick-me-api-staging.herokuapp.com/api/v1',
     API_URL_BACKUP: 'https://pick-me-api-staging.herokuapp.com//api/v1',
     SOCKET_URL: '//apollo-graphql-realtime-chat.herokuapp.com',
-    IMGBB_API_KEY: 'ad7d452dafd347a5b7a743a230740cd4',
+    IMGBB_API_KEY: '78ce20a9868210f47bd14409d19dd78b',
     IMAGE_STORAGE_URL: 'https://api.imgbb.com/1',
     MAP_API_KEY: 'MAP_API_KEY',
 };
