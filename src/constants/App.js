78eef8b73d48110a8239191305103c68bd7f5806
Dung Lu <dung.lu@kyanon.digital>
2022-02-24 07:04:40 +0700

    2202241

--- src/constants/App.js
+++ src/constants/App.js
@@ -1,5 +1,5 @@
 export default {
-    APP_VERSION_OTA: '2202051',
+    APP_VERSION_OTA: '2202241',
     FONT: {
         montserratRegular: 'montserrat-regular',
         montserratBold: 'montserrat-bold',
