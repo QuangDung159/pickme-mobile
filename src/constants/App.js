70333aeb7d049fe222e2370ccf4060c2d0a56601
QDLu <quangdunglu159@gmail.com>
2022-06-23 14:47:10 +0700

    Update 2206236

--- src/constants/App.js
+++ src/constants/App.js
@@ -1,6 +1,6 @@
 export default {
-    APP_VERSION_OTA: '2206235',
-    STORE_VERSION: '1.0.7',
+    APP_VERSION_OTA: '2206236',
+    STORE_VERSION: '1.0.8',
     FONT: {
         montserratRegular: 'montserrat-regular',
         montserratBold: 'montserrat-bold',
